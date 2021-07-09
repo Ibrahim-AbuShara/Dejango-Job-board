a7c6996d48e9cccc005c2c471c15e2b35b5db443
Ibrahim-AbuShara <you@example.com>
2021-07-09 23:26:55 +0200

    elasad ylbes elasad

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import jobs
+from .models import  Category, jobs
 admin.site.register(jobs)
+admin.site.register(Category)
--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,26 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+Job_Type=(('full time','part time'),('part time','full time'))
 # Create your models here.
+class Category(models.Model):
+    cataegoty=models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.cataegoty
+
+
+
+
 class jobs(models.Model):
-    title = models.CharField(max_length=100) #column
+    title=CharField(max_length=50)
+    job_type=CharField(max_length=20, choices=Job_Type)
+    description=models.TextField(max_length=1500)
+    published_at=models.DateTimeField(auto_now=True)
+    vacancy= models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    excprince =models.IntegerField(default=1)
+    category=models.ForeignKey(Category,on_delete=CASCADE)
+
+    def __str__(self) -> str:
+        return self.title

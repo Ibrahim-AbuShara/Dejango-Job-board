d73419241c2838322a9800be416d2827ae48cd68
Ibrahim-AbuShara <you@example.com>
2021-07-13 12:41:36 +0200

    job htmls

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,14 @@
+from typing import ContextManager
 from django.shortcuts import render
-
+from .models import jobs
 # Create your views here.
 def job_list(request):
-    pass
+    job_list=jobs.objects.all()
+    context={"jobs":job_list}
+    return render(request,'jobs/job_list.html',context)
 
 
 def job_detalis(request,id):
-    pass
+   job_detalis=jobs.objects.get(id=id)
+   context={'job':job_detalis}
+   return render(request,'jobs/job_ditales.html',context)

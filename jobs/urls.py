f64bf40cf92b9237c2f1bf6e8c6ba7d7d0514115
Ibrahim-AbuShara <you@example.com>
2021-08-11 23:24:57 +0200

    ben el7ayah wl manga

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
-
+app_name='jobs'
 urlpatterns = [
+    
     path('',views.job_list),
-    path('<int:id>',views.job_detalis),
+    path('<int:id>',views.job_detalis,name='job_detalis'),
     
 ]

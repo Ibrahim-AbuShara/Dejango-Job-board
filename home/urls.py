f51770cdad5a41ef5b69be04d53f8fdb95eb8ae8
Ibrahim-AbuShara <ibrahimaymank27@gmail.com>
2021-09-07 23:46:47 +0200

    adding home ui

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from . import views
+app_name='home'
+urlpatterns = [
+    
+    path('',views.home, name='home'),
+    
+]

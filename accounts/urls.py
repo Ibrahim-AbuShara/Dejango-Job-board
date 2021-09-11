0268ff64e6e39a3b3e9e3b2de11328b20cdb5ae8
Ibrahim-AbuShara <ibrahimaymank27@gmail.com>
2021-09-11 05:21:10 +0200

    add buttons

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
+from django.contrib.auth import  login,logout
 app_name='accounts'
 urlpatterns = [
-    
+    path('login/',login, name='login'),
+    path('logout/',logout, name='logout'),
     path('signup/',views.sign_up, name='signup'),
     path('profile/',views.profile, name='profile'),
     path('editprofile/',views.edit, name='editprofile'),

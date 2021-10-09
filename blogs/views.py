ac2cef948d52d9a3295e4b10208092c1008de8a0
ibrahim <ibrahmaymank27@gmail.com>
2021-10-09 15:27:02 +0200

    bagnitors

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Blog
+from django.core.paginator import Paginator
 def blog_list(request):
     blogs=Blog.objects.all()
-    
-    return render(request, 'blog_list.html',{'blogs':blogs})
+    paginator=Paginator(blogs,2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog_list.html',{'blogs':page_obj})
